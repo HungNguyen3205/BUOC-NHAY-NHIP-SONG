87d73681d0e54ac95664dd7cf386ce224588770a
HungNguyen3205 <158049428+HungNguyen3205@users.noreply.github.com>
2026-09-10 22:06:25 +0700

    Update production Apps Script endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,14 +1,20 @@
 const CONFIG = {
     EVENT_NAME: "BƯỚC CHẠY NHỊP SỐNG",
     EVENT_DATE: "27/09/2026",
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxv3SjmDzRhbwOAMjIhANdDFVuICNzvvIQOcRrEK7Pt6RiomriML75Y3bn3ObyEFu02/exec",
-    BANK_NAME: "MB bank",
+
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw8iD4RS3pW2qkmcNWGAYM1yxtzBI5qOZBNWpAPav2JOGKwfI26zLFHLCVxVx8A5rn6/exec",
+
+    BANK_NAME: "MB Bank",
     BANK_ACCOUNT: "0971120038",
-    ACCOUNT_NAME: "Nguyễn Nam Hùng",
-    QR_IMAGE: "./assets/images/qr-payment.png", // Hoặc assets/images/qr-payment.png
-    SUPPORT_EMAIL: "[EMAIL_ADDRESS]",
+    ACCOUNT_NAME: "NGUYỄN NAM HÙNG",
+
+    QR_IMAGE: "./assets/images/qr-payment.png",
+
+    SUPPORT_EMAIL: "nhipsongbuocchay@gmail.com",
     SUPPORT_PHONE: "0964781078",
+
     FACEBOOK_URL: "https://www.facebook.com/profile.php?id=61593697175095",
+
     FEES: {
         "3km": 150000,
         "5km": 250000,
